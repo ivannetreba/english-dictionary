2b9c3334b989fe8c3c4ab3fbcda77f6b1b33a495
ivannetreba <ivannetreba14@gmail.com>
2024-01-30 01:25:13 +0400

    Build a layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Grid templateAreas={`"nav" "search" "keyword" "noun" "verb" "source"`}>
+      <GridItem area="nav">nav</GridItem>
+      <GridItem area="search">search</GridItem>
+      <GridItem area="keyword">word</GridItem>
+      <GridItem area="noun">noun</GridItem>
+      <GridItem area="verb">verb</GridItem>
+      <GridItem area="source">source</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
